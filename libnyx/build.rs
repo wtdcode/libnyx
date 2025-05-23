39cad5491c8d215bd3847b33ed36458ec070b80c
mio <mio@lazym.io>
2025-05-23 23:20:30 +0800

    build to OUT_DIR

--- libnyx/build.rs
+++ libnyx/build.rs
@@ -4,14 +4,11 @@ use std::path::PathBuf;
 
 
 fn main() {
-    println!("build.rs");
     let crate_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR")
         .expect("CARGO_MANIFEST_DIR env var is not defined"));
-    println!("CARGO_MANIFEST_DIR: {:?}", crate_dir);
 
     let out_dir = PathBuf::from(env::var("OUT_DIR")
         .expect("OUT_DIR env var is not defined"));
-    println!("OUT_DIR: {:?}", out_dir);
 
     let config = cbindgen::Config::from_file("cbindgen.toml")
         .expect("Unable to find cbindgen.toml configuration file");
@@ -20,10 +17,8 @@ fn main() {
     // cargo +nightly build --out-dir `pwd` -Z unstable-options
     // added question to this issue: https://github.com/rust-lang/cargo/issues/6790
     // for now, CARGO_MANIFEST_DIR (crate_dir) seems reasonable
-      
-    
     cbindgen::generate_with_config(&crate_dir, config)
         .unwrap()
-        .write_to_file(crate_dir.join("libnyx.h"));
+        .write_to_file(out_dir.join("libnyx.h"));
     
 }
